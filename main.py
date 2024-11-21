adf67ca5b7a8cf7bac24b89e727db789c8eaae53
maxime-pierrard <maxime.pierrard@edu.esiee.fr>
2024-11-21 08:40:55 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
+"""
+Dans cet exercice on vérifie si une chaine de caractère est un palindrome.
+On va appeller dans une fonction main, une fonction is palindrome qui le vérifie.
+"""
 #### Fonction secondaire
 
 
 def ispalindrome(p):
-
     # votre code ici
-    
-    return False
+    """
+    Cette méthode remplace les caracteres avec des accents par leurs caractères sans accent
+    et elle est transforme toute la string en minuscule et pour finir,
+    elle compare le premier caractere avec le dernier puis le deuxieme avec l'avant dernier etc 
+    """
+    rep=True
+    p=p.lower()
+    accents=str.maketrans("éèêëàâäîïôöùûüç","eeeeaaaiioouuuc"," ,?;.:/!'()-")
+    p=p.translate(accents)
+    x=len(p)
+    for i in range(0,x):
+        a=p[i]
+        b=p[x-i-1]
+        if a!=b:
+            rep=False
+    return rep
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    Cette méthode appelle ispalindrome et affiche le string et son booléen.
+    """
     # vos appels à la fonction secondaire ici
 
-    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
+    for s in ["radar", "kayak", "level", "rotor", "étoile", "Maxime","NINON","RADAR"]:
         print(s, ispalindrome(s))
 
 
